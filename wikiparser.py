c09822ce8cdf876df1ad6836e03ade014fabbb0c
Hadley Callaway <hadleycallaway@dyn-129-236-217-236.dyn.columbia.edu>
2020-03-03 15:21:41 -0500

    g

--- wikiparser.py
+++ wikiparser.py
@@ -9,36 +9,88 @@ import json
 import math
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+import pandas as pd
+from sklearn.cluster import KMeans
+import numpy as np
 
 
-def getRevisions(pageTitle):
-    pageTitle = pageTitle.replace(" ", "_")
-    url = "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&rvlimit=500&titles=" + pageTitle
-    revisions = []                                  #list of all accumulated revisions
-    next = ''                                             #information for the next request
+def get_revisions(page_title):
+    page_title = page_title.replace(" ", "_")
+    url = "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&rvlimit=500&titles=" + page_title
+    revisions = []  # list of all accumulated revisions
+    next_req = ''  # information for the next request
 
     while True:
-        try :
-            response = urllib2.urlopen(url + next).read()     #web request
-            revisions+=(re.findall('<rev [^>]*>', response.decode("utf-8")))  #adds all revisions from the current request to the list
+        try:
+            response = urllib2.urlopen(url + next_req).read()  # web request
+            revisions += (re.findall('<rev [^>]*>', response.decode(
+                "utf-8")))  # adds all revisions from the current request to the list
             cont = re.search('<continue rvcontinue="([^"]+)"', response.decode("utf-8"))
-            if not cont:                                      #break the loop if 'continue' element missing
+            if not cont:  # break the loop if 'continue' element missing
                 break
-            next = "&rvcontinue=" + cont.group(1)             #gets the revision Id from which to start the next request
-        except :
-            return 0 
+            next_req = "&rvcontinue=" + cont.group(1)  # gets the revision Id from which to start the next request
+        except:
+            return 0
 
-    userandtime = getUsersandTime(revisions)
-    neteditscore, velocityscore = getEditScores(userandtime)
-    changes = []
-    changes = getChanges(userandtime)
-    similarites = getSimilarities(changes)
-    return neteditscore, velocityscore, similarites
+    user_and_time = get_users_and_time(revisions)
+    net_edit_score, velocity_score = get_edit_scores(user_and_time)
+    get_velocity(user_and_time)
+    changes = get_changes(user_and_time)
+    similarities = get_similarities(changes)
+    return net_edit_score, velocity_score, similarities
 
 
-def getEditScores(userandtime):
-    neteditscore = 0
-    velocityscore = 0 
+def get_velocity(userandtime):
+    # timecomp = []
+    # prevtime = None
+    # for line in userandtime:
+    #     if prevtime != None:
+    #         dif = prevtime - line[2]
+    #         timecomp.append(dif)
+    #     prevtime = line[2]
+    # print("timecomp", timecomp)
+
+    # get data and create df
+    times = []
+    ids = []
+    for line in userandtime:
+        t = line[2]
+        times.append(t.timestamp())
+        ids.append(line[0])
+    data = {'time': times, 'revid': ids}
+    df = pd.DataFrame(data=data, columns=['time', 'revid'])
+
+    cluster_count = math.floor(math.sqrt(len(userandtime)))
+    # print(userandtime)
+    print("cluster_count", cluster_count)
+    kmeans = KMeans(n_clusters=cluster_count).fit(df)
+    # centroids = kmeans.cluster_centers_
+    # get number of items per cluster
+    # find the time frame of the cluster (subtract latest time from earliest)
+
+    # itemsincluster = []
+    # top = 0
+    scores = []
+    for num in range(0, cluster_count):
+        cluster = np.where(kmeans.labels_ == num)[0]
+        # datetime_cluster = []
+        # for time in cluster:
+        #     datetime_cluster.append(datetime.utcfromtimestamp(time))
+        timeframe = max(cluster) - min(cluster)
+        clusterscore = timeframe / len(cluster)
+        scores.append(clusterscore)
+        # if clusterscore < smallest: smallest = clusterscore
+        # itemsincluster.append(len(cluster))
+    scores.sort()
+    remove = math.ceil(.5 * cluster_count)
+    scores = scores[remove:]
+
+    print("mean", np.mean(scores))  # smaller number = more volatile
+
+
+def get_edit_scores(user_and_time):
+    net_edit_score = 0
+    velocity_score = 0
     delta_hours = timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=6, weeks=0)
     delta_day = timedelta(days=1, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
     delta_week = timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=1)
@@ -47,152 +99,148 @@ def getEditScores(userandtime):
     delta_year = timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=52)
 
     now = datetime.now(tz=None)
-    for line in userandtime:
+    for line in user_and_time:
         if line[2] > (now - delta_hours):
-            velocityscore += 2
+            velocity_score += 2
         elif line[2] > (now - delta_day):
-            velocityscore += 1.5
+            velocity_score += 1.5
         elif line[2] > (now - delta_week):
-            velocityscore += 1.2
+            velocity_score += 1.2
         elif line[2] > (now - delta_month):
-            velocityscore += .2
+            velocity_score += .2
         elif line[2] > (now - delta_halfyear):
-            velocityscore += .15
-        elif line[2] > (now - delta_year):  #LOOK AT EDITS IN WINDOWS SO THAT YOU FIND PAST WINDOWS
-            velocityscore += .1   #could be assigned to past thing
-        else :
-            velocityscore += .01
+            velocity_score += .15
+        elif line[2] > (now - delta_year):  # LOOK AT EDITS IN WINDOWS SO THAT YOU FIND PAST WINDOWS
+            velocity_score += .1  # could be assigned to past thing
+        else:
+            velocity_score += .01
 
         if line[4] == 1:
-            editscore = .5 #if the edit is done by a bot
+            edit_score = .5  # if the edit is done by a bot
         else:
-            editscore = line[3]
+            edit_score = line[3]
 
-        if editscore > 1:
-            editscore = math.sqrt(editscore) #if the edit is done by a normal person
+        if edit_score > 1:
+            edit_score = math.sqrt(edit_score)  # if the edit is done by a normal person
 
-        neteditscore = neteditscore + editscore
-    return neteditscore, velocityscore
+        net_edit_score = net_edit_score + edit_score
+    return net_edit_score, velocity_score
 
 
-def getUsersandTime(revisions):
-    i = 0
-    userandtime = []
-    usercount = {}
+def get_users_and_time(revisions):
+    user_and_time = []
+    user_count = {}
 
     for line in revisions:
         p = line.find('user="')
         p = p + 6
-        p1 = line.find('"',p)
+        p1 = line.find('"', p)
         username = line[p:p1]
         t = line.find('timestamp=')
         t = t + 11
-        t1 = line.find('Z',t)
+        t1 = line.find('Z', t)
         time = line[t:t1]
         time = datetime.fromisoformat(time)
         revise = line.find('revid="')
         revise = revise + 7
-        revise1 = line.find('"',revise)
+        revise1 = line.find('"', revise)
         revid = line[revise:revise1]
         i = 0
-        if re.findall('bot',username):
+        if re.findall('bot', username):
             i = 1
-        if username in usercount:
-            usercount[username] = usercount[username]+1
+        if username in user_count:
+            user_count[username] = user_count[username] + 1
         else:
-            usercount[username] = 1
-        userandtime.append([revid,username,time,usercount[username],i])
-    return userandtime
+            user_count[username] = 1
+        user_and_time.append([revid, username, time, user_count[username], i])
+    return user_and_time
 
 
-def getChanges(userandtime):
-    changesarr = []
+def get_changes(user_and_time):
+    changes_arr = []
     for i in range(100):
-        oldrevid = userandtime[i][0]
-        newrevid = userandtime[i+1][0]
-        oldrevid1 = str(oldrevid)
-        newrevid1 = str(newrevid)
-        url = 'https://en.wikipedia.org/w/api.php?action=compare&format=json&fromrev=' + oldrevid1 +'&torev=' + newrevid1
-        openwebsite = urllib2.urlopen(url)
-        html = openwebsite.read()
-        insertion = parsehtml(html)
-        changes = []
-        changes.append(oldrevid)
-        changes.append(newrevid)
-        changes.append(insertion)
-        changesarr.append(changes)
-    return changesarr
+        oldrevid = user_and_time[i][0]
+        newrevid = user_and_time[i + 1][0]
+        url = 'https://en.wikipedia.org/w/api.php?action=compare&format=json&fromrev=' + str(oldrevid) +\
+              '&torev=' + str(newrevid)
+        open_website = urllib2.urlopen(url)
+        html = open_website.read()
+        insertion = parse_html(html)
+        changes = [oldrevid, newrevid, insertion]
+        changes_arr.append(changes)
+    return changes_arr
 
 
-def getSimilarities(wordarr):
-    count = 0 
-    similarties = 0
-    while count < (len(wordarr)-3):
-        for x in wordarr[count+3]:
+def get_similarities(word_arr):
+    count = 0
+    similarities = 0
+    while count < (len(word_arr) - 3):
+        for x in word_arr[count + 3]:
             for i in range(3):
-                for y in wordarr[count+i]:
+                for y in word_arr[count + i]:
                     if x == y:
-                        similarties += 1
+                        similarities += 1
         count += 1
-    return similarties
+    return similarities
 
 
-def parsehtml(revision_json) :
-    jsonObject = json.loads(revision_json)
-    html = jsonObject["compare"]["*"]
+def parse_html(revision_json):
+    json_object = json.loads(revision_json)
+    html = json_object["compare"]["*"]
     soup_dump = BeautifulSoup(html, 'html.parser')
     changes = soup_dump.find_all('ins', {'class': 'diffchange'})
     out = []
-    
+
     for i in changes:
         i = re.sub('<.*?>', '', str(i))
         i = re.sub(r'([^\s\w]|_)+', '', i)
         i = i.strip()
-        if (i.isspace()) or (len(i) == 0) or (i == None) or (i == ""):
+        if (i.isspace()) or (len(i) == 0) or (i is None) or (i == ""):
             continue
         else:
             out.append(i)
     return out
 
 
-def getKeyWordScore(wiki_title, keywords):
+def get_key_word_score(wiki_title, keywords):
     summary = re.sub(r'[^\w\s]', '', wikipedia.summary(wiki_title)).lower().split()
     score = 0
     for k in keywords:
         if k in summary:
-            score += summary.count(k) #use better search algo
-    return score 
+            score += summary.count(k)  # use better search algo
+    return score
 
 
-def getLinks(wiki_page):
+def get_links(wiki_page):
     return wiki_page.links
 
 
-def runalgo(wiki_title, depth_limit, keywords):
+def run_algo(wiki_title, depth_limit, keywords):
     if depth_limit <= 0:
-        return 
+        return
 
     results = []
     wiki_page = wikipedia.page(wiki_title, None, True, True, False)
-    rev, vscore, similarities = getRevisions(wiki_title)
-    kw_score = getKeyWordScore(wiki_title, keywords)
-    
-    results.append([wiki_title, vscore, kw_score, similarities])
-    links= getLinks(wiki_page)
+    rev, vscore, similarities = get_revisions(wiki_title)
+    kw_score = get_key_word_score(wiki_title, keywords)
 
-    i = 0 
-    while i < 2: 
-        runalgo(links[random.randint(0, len(links)-1)], depth_limit-1, keywords)
+    results.append([wiki_title, vscore, kw_score, similarities])
+    links = get_links(wiki_page)
+
+    i = 0
+    while i < 2:
+        run_algo(links[random.randint(0, len(links) - 1)], depth_limit - 1, keywords)
         i += 1
     return results
 
 
-def parsewiki():
+def parse_wiki():
     wiki_title = input("wiki page title: ")
     depth_limit = 1
-    keywords = ["bad", "china", "good", "quality", "country", "asia", "war", "asian", "east", "power", "democracy", "republic"]
-    results = runalgo(wiki_title, depth_limit, keywords)
+    keywords = ["bad", "china", "good", "quality", "country", "asia", "war", "asian", "east", "power", "democracy",
+                "republic"]
+    results = run_algo(wiki_title, depth_limit, keywords)
     print(results)
 
 
-parsewiki()
+parse_wiki()
